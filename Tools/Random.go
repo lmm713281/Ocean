da6c7b53c7e889a5033c8952b79278e2ccf0fd90
Thorsten Sommer <service@sommer-engineering.com>
2014-11-09 15:04:52 +0100

    UUID
    + Added a lib for generating UUIDs
    + Added a tool function for getting random UUIDs

--- Tools/Random.go
+++ Tools/Random.go
@@ -1,10 +1,17 @@
 package Tools
 
 import (
+	"github.com/twinj/uuid"
 	"math/rand"
 )
 
 func RandomInteger(max int) (rnd int) {
 	rnd = rand.Intn(max)
+	return
+}
+
+func RandomGUID() (guidString string) {
+	guidString = uuid.NewV4().String()
+	guidString = guidString[1 : len(guidString)-1]
 	return
 }
